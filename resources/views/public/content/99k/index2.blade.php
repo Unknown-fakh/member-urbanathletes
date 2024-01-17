1dc673618714e6e996e99ad4e66565a7054a2063
Urban Athletes <itprogrammer@urbanathletes.co.id>
2024-01-17 14:11:32 +0700

    update 99k (6)

--- resources/views/public/content/99k/index2.blade.php
+++ resources/views/public/content/99k/index2.blade.php
@@ -257,7 +257,7 @@
             </li>
             <li class="py-2">
               Fasilitas yang didapat oleh member sama seperti regular membership yaitu : <br>
-              a. Free In-Body Check <br>  c. Free Access All Classes & Exercises <br> e. Free Access kolam renang/sauna.
+              a. Free In-Body Check <br>  c. Free Access All Classes & Exercises  e. Free Access kolam renang/sauna.
               b. Free 1x PT Session   d. Latihan mandiri
             </li>
             <li class="py-2">
